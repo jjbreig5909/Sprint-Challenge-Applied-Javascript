ce4a38bf7e148c0f645300615c61732ebd8cb4c6
Jeff Breig <jjbreig@gmail.com>
2019-11-10 21:53:22 -0500

    topic list functioning

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,36 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        console.log(response);
+        console.log(response.data.topics);
+    })
+    .catch(error => {
+        console.log('the data was not returned', error);
+    });
+
+const newTopic= (topic) =>{
+    const topicName=document.createElement('div');
+    topicName.textContent=`${topic}`;
+    topicName.classList.add('tab');
+return topicName;
+};
+
+const topicList=document.querySelector('.topics');
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        response.data.topics.forEach(topic=> {
+            topicList.appendChild(newTopic(topic))
+        });
+    })
+
+    .catch(error => {
+        console.log('the data was not returned', error);
+    });
